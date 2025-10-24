67399fbf5332c732db29db0af7394ce9e5ca8337
A.ARMIJOS <aarmijosc@a.mikeldi.com>
2025-10-24 13:56:29 +0200

    Creacion de clase Departamento

---
+++ src/main/java/com/reto/erp/model/Departamento.java
@@ -0,0 +1,60 @@
+package com.reto.erp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Departamento")
+public class Departamento {
+	
+	@Id
+	private long id;
+	@Column
+	private String nombre;
+	@Column
+	private String descripcion;
+	@Column
+	private boolean activo;
+	
+	
+	
+	public Departamento() {
+		super();
+	}
+	public Departamento(long id, String nombre, String descripcion, boolean activo) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.activo = activo;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+	
+}
