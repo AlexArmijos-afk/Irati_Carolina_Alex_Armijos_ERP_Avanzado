375c49861eb46608fe0b0d125075b6a065a448ea
A.ARMIJOS <aarmijosc@a.mikeldi.com>
2025-10-24 12:53:55 +0200

    commit inicial

---
+++ src/main/java/com/reto/erp/RetoErpAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.reto.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoErpAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoErpAvanzadoApplication.class, args);
+	}
+
+}
