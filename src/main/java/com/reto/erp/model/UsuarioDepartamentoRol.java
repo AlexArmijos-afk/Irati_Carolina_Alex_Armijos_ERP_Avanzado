92b8b06b0f46f23c048aa5e3b2f4c251175a1902
A.ARMIJOS <aarmijosc@a.mikeldi.com>
2025-10-27 12:50:14 +0100

    creacion de clase UsuarioDepartamentoRolId.java, y puse @-- en UsuarioDepartamentoRol

--- src/main/java/com/reto/erp/model/UsuarioDepartamentoRol.java
+++ src/main/java/com/reto/erp/model/UsuarioDepartamentoRol.java
@@ -1,17 +1,37 @@
 package com.reto.erp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="UsuarioDepartamentoRol")
+@Table(name = "usuario_departamento_rol") // nombre exacto de la tabla en la BD
+@IdClass(UsuarioDepartamentoRolId.class)
 public class UsuarioDepartamentoRol {
 	
-	private Usuario usuario;
-	
-	private Departamento departamento;
-	
-	private Rol rol;
+	@Id
+    private Long usuarioId;
+
+    @Id
+    private Long departamentoId;
+
+    @Id
+    private Long rolId;
+
+    @ManyToOne
+    @JoinColumn(name = "usuarioId", insertable = false, updatable = false)// columna en esta tabla (UsuarioDepartamentoRol) que actúa como FK.
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "departamentoId", insertable = false, updatable = false)
+    private Departamento departamento;
+
+    @ManyToOne
+    @JoinColumn(name = "rolId", insertable = false, updatable = false)
+    private Rol rol;
 	
 	
 }
