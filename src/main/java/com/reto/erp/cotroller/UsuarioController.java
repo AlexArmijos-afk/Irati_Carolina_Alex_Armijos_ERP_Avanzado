08dcda8627ed93e263e32677043cae3c097d3c5d
alex <alex@ALEX>
2025-10-30 21:35:34 +0100

    Cambie cosas para poder navegar entre incio e iniciar sesion, en el css Normal se agrego estilo al boton INGRESAR del inicio

--- src/main/java/com/reto/erp/cotroller/UsuarioController.java
+++ src/main/java/com/reto/erp/cotroller/UsuarioController.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.reto.erp.model.Usuario;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 @RequestMapping("/erp")
 public class UsuarioController {
-	//variable para llamar a registro.html
+	//variable para llamar a inicio.html
 	final String inicio = "inicio";
+	
+	//variable para llamar a iniciarSesion.html
+		final String iniciarSesion = "iniciarSesion";
 		
 	@GetMapping("/bienvenido")
 	public String registrarUsu() {
 		return inicio;
+	}
+	
+	@GetMapping("iniciarSesion")
+	public String putMethodName() {
+		//TODO: process PUT request
+		
+		return iniciarSesion;
 	}
 	
 }
